d64efce8b241bcc11ca09788153be35e809f1dd7
sina <sinaolfati5@gmail.com>
2024-12-19 20:40:32 +0330

    Relearn: Caching

--- app/pages/secondPage/page.tsx
+++ app/pages/secondPage/page.tsx
@@ -1,25 +1,26 @@
-import React from 'react'
+import React from "react";
 
 interface User {
-    id: number;
-    name: string
+  id: number;
+  name: string;
 }
 
-
-
 const SecondPage = async () => {
-
-    const res = await fetch('https://jsonplaceholder.typicode.com/users')
-    const users: User[] = await res.json()
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: { revalidate: 10 }, /* fetches data every 10 seconds - only in fetch and not exios or... */
+  });
+  const users: User[] = await res.json();
 
   return (
     <div>
       Hello everynyan! Welcome to second page
       <ul>
-        {users.map(user => <li key={user.id}>{user.name}</li>)}
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default SecondPage
+export default SecondPage;
