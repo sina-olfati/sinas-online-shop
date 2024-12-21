c57f2c4e98e0a103361454c20792d7d348ab9c68
sina <sinaolfati5@gmail.com>
2024-12-21 22:52:46 +0330

    Next-intl: added src directory

---
+++ src/app/components/theme-mode-toggle.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import { Button } from "@/src/components/ui/button";
+
+export function ThemeModeToggle() {
+  const { setTheme, theme } = useTheme();
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+    >
+      <Sun
+        className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90
+          dark:scale-0"
+      />
+      <Moon
+        className="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-0 transition-all dark:-rotate-0
+          dark:scale-100"
+      />
+    </Button>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import ThemeDataProvider from "@/context/theme-data-provider";
+import ThemeDataProvider from "@/src/context/theme-data-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
