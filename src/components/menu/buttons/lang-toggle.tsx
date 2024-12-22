da5740efe739a9cf5295aefe354cbbc8a924d075
sina <sinaolfati5@gmail.com>
2024-12-23 00:39:49 +0330

    Menu: stuggled with lang toggle's styles

--- src/components/menu/buttons/lang-toggle.tsx
+++ src/components/menu/buttons/lang-toggle.tsx
@@ -19,6 +19,14 @@ export function LangToggle() {
       onClick={() => SaveLocale(locale === "en" ? "jp" : "en")}
       className="hover:bg-[#00000010] dark:hover:bg-[#ffffff10] hover:shadow-sm transition-all"
     >
+      {/* <div className="relative h-[1.2rem] w-[1.2rem] transition-all">
+        <div className={`absolute ${locale === "en" ? "text-lg bottom-[2px] right-[2px]" : "text-xs top-3 left-3"} transition-all`}>
+          EN
+        </div>
+        <div className={`absolute ${locale === "jp" ? "text-lg bottom-[2px] right-[3px]" : "text-xs top-3 left-3"} transition-all`}>
+          JP
+        </div>
+      </div> */}
       <Languages
         className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all"
       />
