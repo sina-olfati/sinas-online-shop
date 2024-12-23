9e77a577bb6baa9ea91e3f3a6e59b6a2de3327a1
sina <sinaolfati5@gmail.com>
2024-12-23 21:55:39 +0330

    Menu: Search completed

--- src/components/menu/search.tsx
+++ src/components/menu/search.tsx
@@ -13,14 +13,13 @@ export function SearchInput() {
                     <label className="w-[100%]">
                         {/* <input type="text" name="name" className="border border-primary rounded-full w-[100%] h-7 outline-0 bg-accent pl-5 focus:" /> */}
                         {/* <Input className="border border-primary rounded-full" /> */}
-                        <Input variant="bordered" className="outline-0 focus:outline-0 rounded-full " />
+                        <Input variant="faded" />
                     </label>
                     {/* <input type="submit" className='absolute right-0 h-full border border-secondary rounded-full mx-2'>
                         {Search}
                     </input> */}
-                    <Button isIconOnly aria-label='search' color="primary" variant="faded" className="absolute right-0 h-full rounded-full">
-                        <Search />
-                        {/* <Search className="text-primary"/> */}
+                    <Button isIconOnly aria-label='search' color="primary" variant="faded" type="submit" className="absolute right-0 border-0 bg-transparent hover:bg-accent scale-[0.95]">
+                        <Search  />
                     </Button>
                 </form>
             </div>
