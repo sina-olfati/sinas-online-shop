5e3c594fac44dc825ac4a36c9762471247af74c4
sina <sinaolfati5@gmail.com>
2024-12-24 23:16:11 +0330

    Menu: BottomSection: phone link completed

--- src/components/menu/bottom/bottomSection.tsx
+++ src/components/menu/bottom/bottomSection.tsx
@@ -1,6 +1,8 @@
 "use client";
+import Link from "next/link";
 import { Button } from "../../ui/button";
 import { DropDownButton } from "./dropDownButton";
+import { Phone } from "lucide-react";
 
 const normalButtons = ["Questions", "Articles", "About Us"];
 
@@ -78,22 +80,28 @@ const moreData = {
 export function BottomSection() {
   return (
     <div className="h-full w-full">
-      <div className="flex flex-shrink-0 h-full w-full">
+      <div className="flex flex-shrink-0 justify-between h-full w-full px-8">
 
-        <div>
-        </div>
+        <div about="buttons" className="flex items-center justify-start gap-2 flex-shrink-0 h-full">
 
-        <div className="flex items-center justify-start gap-2 h-full w-full">
             <DropDownButton name={seasonsData.name} headers={seasonsData.headers} items={seasonsData.items}/>
             <DropDownButton name={moreData.name} headers={moreData.headers} items={moreData.items}/>
+
             {normalButtons.map((name) => (
-                <Button
-                key={name}
-                variant="ghost"
-                className="hover:bg-[#00000010] dark:hover:bg-[#ffffff10] hover:shadow-sm transition-all text-xs">
+                <Button key={name} variant="ghost" className="hover:bg-[#00000010] dark:hover:bg-[#ffffff10] hover:shadow-sm transition-all text-xs">
                 {name}
                 </Button>
             ))}
+
+        </div>
+
+        <div about="phone" className="">
+            <Link href="tel:+98 994 166 9736">
+                <Button variant={"ghost"} className="group hover:bg-[#00000010] dark:hover:bg-[#ffffff10] hover:shadow-sm transition-all text-xs">
+                    <Phone className="group-hover:text-primary" />
+                    <p>+98-9941669736</p>
+                </Button>
+            </Link>
         </div>
 
       </div>
