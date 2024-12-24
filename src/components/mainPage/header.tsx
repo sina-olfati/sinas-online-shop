d090e6ad1da69c43f40aa599754514d9b7a8fd8b
sina <sinaolfati5@gmail.com>
2024-12-25 00:52:16 +0330

    Header: added buttons

--- src/components/mainPage/header.tsx
+++ src/components/mainPage/header.tsx
@@ -1,6 +1,7 @@
 'use client'
-import Image from "next/image";
 import { useEffect, useState } from "react";
+import Image from "next/image";
+import { Button } from "../ui/button";
 
 export function Header() {
 
@@ -18,13 +19,15 @@ export function Header() {
 
     return (
         // <div className="border border-primary w-full overflow-hidden">
-        <div className="border border-primary w-full">
+        <div className="w-full overflow-x-hidden relative">
+
             <div className="flex flex-shrink-0 h-52 w-[400vw]">
 
-                <div className={`${turn === 1 ? "translate-x-[0vw] !transition-none" :
+                <div about="scroller" className={`${turn === 1 ? "translate-x-[0vw] !transition-none" :
                     turn === 2 ? "translate-x-[-100vw]" : 
                     turn === 3 ? "translate-x-[-200vw]" : "translate-x-[-300vw]"
                 } w-[400vw] h-full flex flex-shrink-0 relative transition-all`}>
+
                     <div className="bg-sky-500 w-[100vw] h-full"></div>
                     <div className="bg-red-500 w-[100vw] h-full"></div>
                     <div className="bg-green-500 w-[100vw] h-full"></div>
@@ -32,11 +35,17 @@ export function Header() {
                     {/* <Image fill={true} src={"../../../public/globe.svg"} className="bg-sky-500" alt="Shop Product Image" />
                     <Image fill={true} src={"../../../public/globe.svg"} className="bg-green-500" alt="Shop Product Image" />
                     <Image fill={true} src={"../../../public/globe.svg"} className="bg-blue-500" alt="Shop Product Image" /> */}
+
                 </div>
 
-                <div></div>
-
             </div>
+
+            <div about="buttons" className="border border-primary absolute bottom-0 w-full flex justify-center gap-2 mb-2" >
+                <Button variant="ghost" className={`w-2 h-2 p-0 rounded-full transition-all bg-accent hover:bg-primary ${turn === 1 || turn === 4 ? 'bg-primary w-7' : null}`}></Button>
+                <Button variant="ghost" className={`w-2 h-2 p-0 rounded-full transition-all bg-accent hover:bg-primary ${turn === 2 ? 'bg-primary w-7' : null}`}></Button>
+                <Button variant="ghost" className={`w-2 h-2 p-0 rounded-full transition-all bg-accent hover:bg-primary ${turn === 3 ? 'bg-primary w-7' : null}`}></Button>
+            </div>
+
         </div>
     )
 }
