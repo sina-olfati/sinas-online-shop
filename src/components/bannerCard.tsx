77bf05ef79fbf6dd2d7c7542b97c057a40d62d13
sina <sinaolfati5@gmail.com>
2024-12-27 23:31:02 +0330

    Added banners images

--- src/components/bannerCard.tsx
+++ src/components/bannerCard.tsx
@@ -1,17 +1,18 @@
 'use client'
-import { Image } from "@nextui-org/react"
+import { Button, Image } from "@nextui-org/react"
+import Link from "next/link"
 import { inherits } from "util"
 
 
 const data = [
     {
         id:1,
-        name: 'something',
+        name: 'Every gentleman should have these',
         picture: null,
     },
     {
         id:2,
-        name: 'something2',
+        name: 'Ladies essentials',
         picture: null,
     }
 ]
@@ -23,14 +24,18 @@ export function BannerCard () {
 
             {data.map((card) => 
                 // <div key={card.id} className="bg-['./productImages/hoody1.webp'] bg-cover bg-center">
-                <div key={card.id} className="bg-primary w-full h-64 rounded-lg flex flex-col justify-between items-start px-8 py-6"> 
-                    {/* <Image 
+                <div key={card.id} className="bg-muted-foreground w-full h-64 rounded-lg flex flex-col justify-between items-start px-8 py-6"> 
+                    <Image 
                         width={100} 
-                        src={'./productImages/hoody1.webp'} 
+                        src={'./banners/lady1.jpg'} 
                         alt="banner card"
                         className="!w-full absolute inset-0"
-                    /> */}
-                    <h2>{card.name}</h2>
+                    />
+                    <h2 className="font-bold text-xl text-background">{card.name}</h2>
+
+                    <Link href={'./ss'}>
+                        <Button variant="shadow" color="primary" className="font-bold">Start Shopping</Button>
+                    </Link>
                 </div>
             )}
 
