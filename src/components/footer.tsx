03d9ec62738c2a8b9997cfa742c6278e8c13a0c4
sina <sinaolfati5@gmail.com>
2024-12-29 00:54:35 +0330

    Footer: completed main section

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,6 +2,36 @@ import Link from "next/link";
 import { Logo } from "./menu/logo";
 import { Diamond } from "lucide-react";
 // icons
+import { Send, Phone, Linkedin, Mail, Github } from "lucide-react";
+
+
+const socials = [
+    {
+        name: "mail",
+        link: "mailto:sinaolfati6@gmail.com",
+        icon: <Mail className="group-hover:text-primary transition-all w-5" />
+    },
+    {
+        name: "linkedin",
+        link: "https://www.linkedin.com/in/sina-olfati-872950233",
+        icon: <Linkedin className="group-hover:text-primary transition-all w-5" />
+    },
+    {
+        name: "github",
+        link: "https://github.com/sina-olfati",
+        icon: <Github className="group-hover:text-primary transition-all w-5" />
+    },
+    {
+        name: "phone",
+        link: "tel:+989941669736",
+        icon: <Phone className="group-hover:text-primary transition-all w-5" />
+    },
+    {
+        name: "telegram",
+        link: "https://t.me/sinaolfati",
+        icon: <Send className="group-hover:text-primary transition-all w-5" />
+    }
+];
 
 
 const lists = [
@@ -38,23 +68,20 @@ export function Footer () {
             <div about="main" className="flex items-start gap-20 p-16 pt-32">
 
                 {/* Logo and Tagline */}
-                <div className="w-[31%] pr-10 h-full flex flex-col items-start justify-start">
-                    <Logo />
+                <div className="w-[31%] pr-10 h-full flex flex-col items-start justify-start gap-4">
+                    <div className="scale-150 h-10 relative r-5">
+                        <Logo />
+                    </div>
                     <p className="mt-4 text-sm">Elevate your style with our exclusive collection of men's and women's fashion. Discover the perfect pieces to complement your wardrobe and make a lasting impression.</p>
 
                     <div about="social" className="mt-4 flex gap-4">
-                        <Link href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-                            <img src="/icons/facebook.svg" alt="Facebook" className="w-6 h-6" />
-                        </Link>
-                        <Link href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-                            <img src="/icons/instagram.svg" alt="Instagram" className="w-6 h-6" />
-                        </Link>
-                        <Link href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-                            <img src="/icons/twitter.svg" alt="Twitter" className="w-6 h-6" />
-                        </Link>
-                        <Link href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-                            <img src="/icons/linkedin.svg" alt="LinkedIn" className="w-6 h-6" />
-                        </Link>
+
+                        {socials.map((social) => 
+                            <Link key={social.name} href={social.link} target="_blank" rel="noopener noreferrer" className="group">
+                                {social.icon}
+                            </Link>
+                        )}
+
                     </div>
                 </div>
 
@@ -65,7 +92,7 @@ export function Footer () {
                             {list.map((link, index) => (
                                 <Link  key={index} href={link.href}>
                                     <li className="flex gap-2 items-center group transition-all">
-                                        <Diamond strokeWidth={4} width={15} fill="transparent" className="text-primary group-hover:fill-primary transition-all" />
+                                        <Diamond strokeWidth={4} width={13} fill="transparent" className="text-primary group-hover:fill-primary transition-all" />
                                         {link.name}
                                     </li>
                                 </Link>
