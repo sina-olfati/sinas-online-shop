77a007c7e20e9b46f37b503ee3d5fd302e76bd3b
sina <sinaolfati5@gmail.com>
2025-01-01 01:41:21 +0330

    Filters: made it sticky

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,13 +4,15 @@ import { Filters } from "@/src/components/productsPage/filters/filters";
 export default function Products() {
   return (
     <div className="mt-24 flex items-start justify-center gap-10 p-10">
-      <div about="filters" className="w-80 h-full flex shrink-0">
+
+      <div about="filters" className="sticky top-24 w-80 h-full flex shrink-0">
         <Filters />
       </div>
 
       <div about="products" className="w-full h-full">
         <AllProducts />
       </div>
+
     </div>
   );
 }
