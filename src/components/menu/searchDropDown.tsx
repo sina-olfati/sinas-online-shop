5326f870e4322f4a88d788d1b6f36926efc324b0
sina <sinaolfati5@gmail.com>
2025-01-02 22:27:33 +0330

    SearchDropDown: completed

--- src/components/menu/searchDropDown.tsx
+++ src/components/menu/searchDropDown.tsx
@@ -1,4 +1,6 @@
-
+import Link from "next/link"
+import Products from "../../../data/products.json"
+import Image from "next/image"
 const items = [
     1,2,3,4,5,6,7
 ]
@@ -7,12 +9,25 @@ const items = [
 export function SearchDropDown () {
 
     return (
-        <div className="border border-primary absolute w-full mt-2 overflow-auto rounded-2xl max-h-52 bg-secondary z-50 flexflex-col">
-            {items.map((product) => 
-                <div key={product} className="border border-primary p-3">
-                    name
-                </div>
+        <div className="absolute w-full mt-2 shadow overflow-auto rounded-2xl max-h-52 bg-primary-foreground z-50 flex flex-col transition-all">
+            {Products.map((product) => 
+                <Link href={"./"}  key={product.id}  className="hover:bg-secondary-foreground/5 px-5">
+                    <div className="flex flex-col">
+
+                        <div className="py-2 flex gap-3 items-center">
+                            <Image className="w-12" src={product.images[0]} width={48} height={48} alt={`product ${product.name} image`} />
+                            <div className="flex flex-col gap-1 truncate">
+                                <h4 className="text-sm font-semibold">{product.name}</h4>
+                                <p className="text-xs opacity-80">In <i className="text-primary not-italic">{product.category}</i></p>
+                            </div>
+                        </div>
+
+                        <div className="w-[95%] mx-auto border border-x-0 border-t-0 border-b-secondary-foreground/10"></div>
+                    </div>
+                </Link>
             )}
+
+            {/* <div className="fixed inset-0 z-20 bg-black/50"></div> */}
         </div>
     )
 }
