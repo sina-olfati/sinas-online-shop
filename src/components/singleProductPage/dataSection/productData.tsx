feda7c5e0ca8967e353819910eb369e81712c9b2
sina <sinaolfati5@gmail.com>
2025-01-04 00:01:07 +0330

    ProductData: made the details section

--- src/components/singleProductPage/dataSection/productData.tsx
+++ src/components/singleProductPage/dataSection/productData.tsx
@@ -1,4 +1,6 @@
+import { Button, Card, CardBody, Tab, Tabs } from "@nextui-org/react";
 import { DataHeader } from "./dataHeader";
+import { useState } from "react";
 
 interface Product {
     id: number;
@@ -21,11 +23,70 @@ interface ProductDataProps {
     product: Product;
 }
 
+
+
+
+
 export function ProductData ({product}: ProductDataProps) {
+    
+    const [show, setShow] = useState("details")
+    
+    const details = [
+        {
+            name: "Category",
+            answer: product.category
+        },
+        {
+            name: "Brand",
+            answer: product.brand
+        },
+        {
+            name: "Fabric Type", // Changed for clarity
+            answer: product.fabric_type
+        },
+        {
+            name: "Season", // Ensure consistent capitalization
+            answer: product.season // Updated to match lowercase convention
+        },
+        {
+            name: "Target Audience", // More descriptive than "Gender"
+            answer: product.gender
+        }
+    ]
+
 
     return (
-        <div className="">
+        <div className="flex flex-col">
             <DataHeader product={product} />
+
+            <div className="border border-primary flex w-full flex-col my-10">
+
+                <Tabs aria-label="Options">
+                    <Tab key="details" title="Details">
+                        <Card shadow="none" className="shadow rounded-md">
+                            <CardBody className="bg-secondary">
+                                <ul>
+                                    {details.map((data) => 
+                                        <li key={data.name} className="flex gap-2 p-3">
+                                            <h3 className="font-semibold">{data.name}:</h3>
+                                            <p>{data.answer}</p>
+                                        </li>
+                                    )}
+                                </ul>
+                            </CardBody>
+                        </Card>
+                    </Tab>
+                    <Tab key="comments" title="Comments">
+                        <Card>
+                            <CardBody>
+                                comments
+                            </CardBody>
+                        </Card>
+                    </Tab>
+                </Tabs>
+               
+            </div>
+
         </div>
     )
 }
