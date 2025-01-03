36eff525cb6810e790fae08df41734911ddc3f54
sina <sinaolfati5@gmail.com>
2025-01-03 23:30:54 +0330

    AddToCartButotn: styled

--- src/components/addToCartButton.tsx
+++ src/components/addToCartButton.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@nextui-org/react"
 // import { Button } from "./ui/button"
 // icons
-import { ShoppingCart } from "lucide-react"
+import { ArrowDownToDot, Minus, Plus, ShoppingCart, Trash, Trash2 } from "lucide-react"
 import { useState } from "react"
 
 export function AddToCartButton () {
@@ -9,17 +9,45 @@ export function AddToCartButton () {
     const [buy, setBuy] = useState(0)
 
     return (
-        <div className="border border-primary relative">
+        <div className="relative flex items-center justify-center">
 
-            <Button 
-            isIconOnly 
-            color="primary" 
-            variant="flat" 
-            className="text-secondary-foreground h-5 transition-all"
-            onPress={() => setBuy(1)}
-            >
-                {buy ? buy : <ShoppingCart /> }
-            </Button>
+            {!buy ?
+                <Button 
+                color="primary" 
+                variant="shadow" 
+                className="transition-all w-[60%] py-7 my-5 font-semibold"
+                onPress={() => setBuy(1)}
+                >
+                    {buy ? buy : <ShoppingCart /> }
+                    Add To Cart
+                </Button>
+            : 
+                <div className="transition-all w-[60%] my-5 bg-secondary-foreground/10 rounded-xl flex items-center justify-between">
+
+                    <Button 
+                        isIconOnly
+                        color={buy === 1 ? "danger" : "primary"} 
+                        variant="solid" 
+                        className="transition-all h-14 w-14 rounded-xl rounded-r-none"
+                        onPress={() => setBuy(buy-1)}
+                    >
+                        {buy === 1 ? <Trash2 /> : <Minus /> }
+                    </Button>
+
+                    <p>{buy}</p>
+
+                    <Button 
+                        isIconOnly
+                        color="primary" 
+                        variant="solid" 
+                        className="transition-all h-14 w-14 rounded-xl rounded-l-none"
+                        onPress={() => setBuy(buy+1)}
+                    >
+                        <Plus />
+                    </Button>
+
+                </div>
+            }
             
         </div>
     )
